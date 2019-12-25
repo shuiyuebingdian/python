d455d222a0f401a99d442cc5e18edcf22fe52fc4
sunshaoyan <yanfeel2009@163.com>
2019-12-25 16:47:14 +0800

    plus one

--- algorithm/FibonacciSequence.py
+++ algorithm/FibonacciSequence.py
@@ -7,14 +7,15 @@ def fbnq(num):
     """
     列出num位个斐波那契数列
     """
-    arr=[]
+    arr = []
     a, b = 0, 1
     for i in range(num):
         arr.append(a)
         a, b = b, a + b
-    
+
     print(arr)
-        
+
+
 print("10位斐波那契数列")
 fbnq(10)
 print()
@@ -28,24 +29,28 @@ def fbnq_item(num):
     for i in range(num - 1):
         a, b = b, a + b
     return a
+
+
 print("第10位斐波那契数列元素")
 print(fbnq_item(10))
 print()
 
-
 """
 官网示例
 """
+
+
 def fbnq_from_offical_website(num):
     a, b = 0, 1
     while a < num:
         print(a, end=",")
         a, b = b, a + b
-        
+
 
 print("从0开始，小于10的斐波那契数列")
 fbnq_from_offical_website(10)
 print()
+
 
 def fbnq_from_offical_website(num):
     a, b = 0, 1
@@ -53,7 +58,7 @@ def fbnq_from_offical_website(num):
         print(a, end=",")
         a, b = b, a + b
         num = num - 1
-        
+
 
 print("\n从0开始，10位的斐波那契数列")
 fbnq_from_offical_website(10)
